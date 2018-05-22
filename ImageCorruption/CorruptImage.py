db489e75176ff48a38abafd29d9c9b31fe2d80fe
有明 雍 <40241146@gm.nfu.edu.tw>
2018-05-23 03:03:27 +0800

    basic PEP8 fix

--- ImageCorruption/CorruptImage.py
+++ ImageCorruption/CorruptImage.py
@@ -1,61 +1,63 @@
 
 from PIL import Image
-from numpy import array
 from random import randint
 import numpy
 
 
+def convertToJpg(file):
+    rgb_im = file.convert('RGB')
+    return rgb_im
 
 
-##################Image Process################
 def SaveImageFromArray(arr):
     img = Image.fromarray(arr)
     img.save("outTestImage.png")
-def corrput(x,y,z,arr,flag):
-    nval=arr[x][y][z]
-    if(flag==0):
-        nval+=100
-        if(nval>=255):
-            arr[x][y][z]=255
-        else:
-            arr[x][y][z]=nval
-    else:
-        nval-=100
-        if(nval<=0):
-            arr[x][y][z]=0
-        else:
-            arr[x][y][z]=nval
 
+
+def checkCorrupted(arr, originArr):
+
+    cnt = 0
+    for i in range(460):
+        for j in range(833):
+            for k in range(3):
+                originValue = originArr[i][j][k]
+                newValue = arr[i][j][k]
+
+                if newValue != originValue:
+                    cnt += 1
+                    print(str(cnt)+".the differnce : Origin["+str(originValue)+"], New["+str(newValue)+"]")
+
+
+def corrupt(x, y, z, arr, flag):
+    newValue = arr[x][y][z]
+    if flag == 0:
+        newValue += 100
+        if newValue >= 255:
+            arr[x][y][z] = 255
+        else:
+            arr[x][y][z] = newValue
+    else:
+        newValue -= 100
+        if newValue <= 0:
+            arr[x][y][z] = 0
+        else:
+            arr[x][y][z] = newValue
 
 
 def main():
-
     img = Image.open("testImg.png")
-    arr = numpy.array(img)
-    originArr = numpy.array(img)
+    jpgImage = convertToJpg(img)
+    arr = numpy.array(jpgImage)
+    originArr = numpy.array(jpgImage)
 
-    for i in range(0,1000):
-        randX=randint(0,459)
-        randY=randint(0,832)
-        randZ=randint(0,3)
+    for i in range(0, 1000):
+        randX = randint(0, 459)
+        randY = randint(0, 832)
+        randZ = randint(0, 2)
 
-        corrput(randX,randY,randZ,arr,i%2)
+        corrupt(randX, randY, randZ, arr, i % 2)
     im = Image.fromarray(arr)
     im.save("outTestImage.png")
-
-    #this is checking
-    #cnt=0
-    # for i in range(460):
-    #     for j in range(833):
-    #         for k in range(4):
-    #             oVal=originArr[i][j][k]
-    #             nVal=arr[i][j][k]
-    #
-    #             if(nVal!=oVal):
-    #                 cnt+=1
-    #                 print(str(cnt)+".the differnce : Origin["+str(oVal)+"], New["+str(nVal)+"]")
-    #
-
 
 
 if __name__ == '__main__':
