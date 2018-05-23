d64dc86a234b17fb5187b5ea0008043e57d928f6
有明 雍 <40241146@gm.nfu.edu.tw>
2018-05-24 00:24:58 +0800

    corrupt by rate

--- ImageCorruption/CorruptImage.py
+++ ImageCorruption/CorruptImage.py
@@ -14,12 +14,12 @@ def SaveImageFromArray(arr):
     img.save("outTestImage.png")
 
 
-def checkCorrupted(arr, originArr):
+def checkCorrupted(arr, originArr, x, y, z):
 
     cnt = 0
-    for i in range(460):
-        for j in range(833):
-            for k in range(3):
+    for i in range(x):
+        for j in range(y):
+            for k in range(z):
                 originValue = originArr[i][j][k]
                 newValue = arr[i][j][k]
 
@@ -28,7 +28,7 @@ def checkCorrupted(arr, originArr):
                     print(str(cnt)+".the differnce : Origin["+str(originValue)+"], New["+str(newValue)+"]")
 
 
-def corrupt(x, y, z, arr, flag):
+def fill(x, y, z, arr, flag):
     newValue = arr[x][y][z]
     if flag == 0:
         newValue += 100
@@ -44,21 +44,40 @@ def corrupt(x, y, z, arr, flag):
             arr[x][y][z] = newValue
 
 
-def main():
+def execute(rate):
     img = Image.open("testImg.png")
     jpgImage = convertToJpg(img)
     arr = numpy.array(jpgImage)
     originArr = numpy.array(jpgImage)
 
-    for i in range(0, 1000):
-        randX = randint(0, 459)
-        randY = randint(0, 832)
-        randZ = randint(0, 2)
+    dim_x = numpy.size(arr, 0)
+    dim_y = numpy.size(arr, 1)
+    dim_z = numpy.size(arr, 2)
 
-        corrupt(randX, randY, randZ, arr, i % 2)
+    #make sure polluted dots is plural
+    times_double = arr.size * rate / 100
+    times_int = 0
+    if round(times_double) % 2 != 0:
+        times_int = round(times_double)-1
+    else:
+        times_int = round(times_double)
+
+    newProcess = 0
+    for i in range(0, times_int):
+        randX = randint(0, dim_x - 1)
+        randY = randint(0, dim_y - 1)
+        randZ = randint(0, dim_z - 1)
+        fill(randX, randY, randZ, arr, i % 2)
+
+        nowProcess = round(i / times_double * 100)
+
+
+        if newProcess != nowProcess:
+            print(str(newProcess) + "%")
+        newProcess = nowProcess
+
+
     im = Image.fromarray(arr)
     im.save("outTestImage.png")
+    #checkCorrupted(arr, originArr, dim_x, dim_y, dim_z)
 
-
-if __name__ == '__main__':
-    main()
---
+++ ImageCorruption/MainProgram.py
@@ -0,0 +1,11 @@
+import CorruptImage as corruption
+import numpy
+
+def main():
+
+    corruption.execute(100)
+    print('[step1. Image Corrupted]')
+
+
+if __name__ == '__main__':
+        main()
